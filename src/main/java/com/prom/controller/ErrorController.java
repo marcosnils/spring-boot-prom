13442e453c696774da85943a033ac442fc3dd92b
Marcos Lilljedahl <marcosnils@gmail.com>
2018-03-21 12:39:30 -0300

    Replace spring-metrics with micrometer

--- src/main/java/com/prom/controller/ErrorController.java
+++ src/main/java/com/prom/controller/ErrorController.java
@@ -1,8 +1,8 @@
 package com.prom.controller;
 
+import io.micrometer.core.annotation.Timed;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.metrics.annotation.Timed;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
